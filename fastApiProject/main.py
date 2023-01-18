2bcc5c401d41ef063dc40adc4e108d59f3320111
Taha <91777330+tahamukhtar20@users.noreply.github.com>
2023-01-19 03:30:00 +0500

    Web Scraping almost done, api stuff and forms integration left

--- fastApiProject/main.py
+++ fastApiProject/main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI
+from test import data_function
 
 app = FastAPI()
 
 
-@app.get("/")
-async def test():
-    return {"message": "Hello World"}
-
-
-@app.get("/login")
-async def login():
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/formfill")
-async def form():
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/predictor")
-async def grade_predict():
-    return {"message": f"Hello {name}"}
+@app.get("/dataRequest")
+async def data_req():
+    return data_function()
--- fastApiProject/test.py
+++ fastApiProject/test.py
@@ -1,16 +1,18 @@
-from bs4 import BeautifulSoup
+import re
 import requests
+from bs4 import BeautifulSoup
 
 
 class Scrape:
-
     def __init__(self, username, password):
-        self.url_login = "https://qalam.nust.edu.pk/web/login"
-        self.url_form = "https://qalam.nust.edu.pk/student/dashboard"
-        # self.url_predictor = ""
+        self.url_base, self.url_login, self.url_aggregate, self.url_result = (
+            "https://qalam.nust.edu.pk/",
+            "https://qalam.nust.edu.pk/web/login",
+            "https://qalam.nust.edu.pk/student/dashboard",
+            "https://qalam.nust.edu.pk/student/results/id/"
+        )
         self.session = requests.Session()
-        self.username = username
-        self.password = password
+        self.username, self.password = username, password
         self.csrf_token = lambda x: x.find("input", {"name": "csrf_token"})["value"]
 
     def auth(self):
@@ -19,12 +21,68 @@ class Scrape:
         csrf_token = self.csrf_token(soup)
         login_data = {"csrf_token": csrf_token, "login": self.username, "password": self.password}
         r = self.session.post(self.url_login, data=login_data)
-        if r.status_code != 200:
-            print("Login Failed")
-            return False
-        return True
+        return r.status_code == 200
 
-    def 
-if __name__ == "__main__":
+    def dashboard(self):
+        r = self.session.get(self.url_aggregate)
+        soup = BeautifulSoup(r.content, "html.parser")
+        data = soup.find_all("div", {
+            "class": ["uk-grid", "uk-grid-width-small-1-12", "uk-grid-width-medium-1-12", "uk-grid-width-medium-1-12",
+                      "uk-grid-width-large-1-4", "uk-margin-medium-bottom"], "data-uk-grid-margin": "",
+            "id": "hierarchical-show", "data-show-delay": "100"})[1].find_all("a")
+
+        datajson = {}
+        for i in data:
+            datajson[i.find("span", {"class": "md-list-heading md-color-grey-900"}).text] = [
+                self.url_base +
+                re.findall(r'''<a data-uk-tooltip="{pos:'top'}" href="(.*?)" title="Open class">''', i.prettify())[0],
+                i.find("span", {"class": "md-list-heading md-color-blue-grey-600"}).text,
+                i.find("span", {"class": "sub-heading md-color-blue-grey-600"}).text
+            ]
+        return datajson
+
+    def result(self, url):
+        r = self.session.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        table = soup.findAll("table", {"class": "uk-table uk-table-nowrap uk-table-align-vertical table_tree"})
+        table = str(table[0]) + str(table[1])
+        table = BeautifulSoup(table, "html.parser")
+        data = {}
+        temp = []
+        tempVar = ""
+        for i in table.findAll("tr"):
+            if i.text not in ['\nAssessment Type\nObtained Percentage\n',
+                              '\nAssessment\nMax Mark\nObtained Marks\nClass Average\nPercentage\n']:
+                x = i.text
+                temp.append(" ".join(x.split()))
+        for j in temp:
+            if not re.search(r'\d+(\.\d+)?\s+', j):
+                tempVar = j
+                data[tempVar] = []
+            else:
+                data[tempVar].append(j.split())
+
+        for ke, val in data.items():
+            temp_test = {}
+            for kx in val:
+                while len(kx) > 5:
+                    kx[0] += kx[1]
+                    kx.pop(1)
+                temp_test[kx[0]] = {
+                    "maxMark": kx[1],
+                    "obtained": kx[2],
+                    "classAvg": kx[3],
+                    "percentage": kx[4]
+                }
+            data[ke] = temp_test
+        return data
+
+
+def data_function():
     scraper = Scrape("madeel.bscs21seecs", "Student.123")
     scraper.auth()
+    print(scraper.dashboard())
+
+
+if __name__ == "__main__":
+    print(data_function())
